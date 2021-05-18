6be46b1b496a0b1600e4a9affcad648ac4d1e068
Ranveer Shah <ranveer2001s@gmail.com>
2021-05-18 18:16:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './Components/Form';
+import List from './Components/List';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>To-do</h1>
+      <List/>
+    </div>
+  );
+}
+
+export default App;

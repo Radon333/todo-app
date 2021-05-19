822895c0d868c82949486bec57b8ff334b0ffbdd
Radon333 <ranveer2001s@gmail.com>
2021-05-19 20:44:01 +0530

    Updates

---
+++ precache-manifest.f0d50b4b93119e460f48e453cd96b46e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645c5cd9fdfd783d109a32cf6c7b2400",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "f13afc2b5e3836a5f7fd",
+    "url": "/todo-app/static/css/main.0520c85b.chunk.css"
+  },
+  {
+    "revision": "832f1ac8fdd3895b3fc9",
+    "url": "/todo-app/static/js/2.ae3eede5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-app/static/js/2.ae3eede5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13afc2b5e3836a5f7fd",
+    "url": "/todo-app/static/js/main.67a1f564.chunk.js"
+  },
+  {
+    "revision": "32ffbcb42511e8498550",
+    "url": "/todo-app/static/js/runtime-main.2d192a4e.js"
+  }
+]);
